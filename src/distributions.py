ab45945e08cafbd4b19b4be99b91e67cc95766e5
Ivo Petrov <1414vo@gmail.com>
2024-03-09 21:20:44 +0000

    Refactored posterior and likelihood computations

--- src/distributions.py
+++ src/distributions.py
@@ -29,6 +29,25 @@ def simple_posterior(
     return np.nan_to_num(likelihood + prior_alpha + prior_beta, nan=-np.inf)
 
 
+def intensity_likelihood(x, log_i, alpha, beta, i_0):
+    """!Defines the log-likelihood for the detection problem,
+    including information about the light intensity.
+
+    @param x            A set of observed light detections.
+    @param log_i        A set of observed log-intensities.
+    @param alpha        The location of the lighthouse.
+    @param beta         The height of the lighthouse.
+    @param i_0          The intensity at the lighthouse origin.
+
+    @returns            The log-likelihood for the location, height and intensity.
+    """
+    likelihood_location = stats.cauchy.logpdf(x, loc=alpha, scale=beta)
+    d = beta**2 + (x - alpha) ** 2
+    likelihood_intensity = stats.norm.logpdf(log_i, loc=np.log(i_0 / d), scale=1)
+
+    return likelihood_location + likelihood_intensity
+
+
 def intensity_posterior(
     x,
     log_i,
@@ -42,7 +61,8 @@ def intensity_posterior(
     i_min=1e-5,
     i_max=100,
 ):
-    """!Defines the non-normalized posterior for the simple detection problem.
+    """!Defines the non-normalized posterior for the detection problem,
+    including information about the light intensity.
 
     @param x            A set of observed light detections.
     @param log_i        A set of observed log-intensities.
@@ -56,12 +76,10 @@ def intensity_posterior(
     @param i_min        The lower bound of the lighthouse intensity.
     @param i_max        The upper bound of the lighthouse intensity.
 
-    @returns            A posterior probability density for the location, height and intensity.
+    @returns            A log of the posterior probability density for the
+    location, height and intensity.
     """
-
-    likelihood_location = stats.cauchy.logpdf(x, loc=alpha, scale=beta)
-    d = beta**2 + (x - alpha) ** 2
-    likelihood_intensity = stats.norm.logpdf(log_i, loc=np.log(i_0 / d), scale=1)
+    likelihood = intensity_likelihood(x, log_i, alpha, beta, i_0)
 
     # Compute the 3 components of the prior
     prior_alpha = stats.uniform.logpdf(
@@ -70,6 +88,4 @@ def intensity_posterior(
     prior_beta = stats.uniform.logpdf(beta, loc=beta_min, scale=beta_max - beta_min)
     prior_i = stats.loguniform.logpdf(i_0, a=i_min, b=i_max)
 
-    return (
-        likelihood_location + likelihood_intensity + prior_alpha + prior_beta + prior_i
-    )
+    return likelihood + prior_alpha + prior_beta + prior_i
