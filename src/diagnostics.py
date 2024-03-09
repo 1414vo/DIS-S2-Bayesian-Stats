7d076da8804b3b974222265fc40143e54d083bc1
Ivo Petrov <1414vo@gmail.com>
2024-03-09 14:19:50 +0000

    KL-Divergence metrics

--- src/diagnostics.py
+++ src/diagnostics.py
@@ -1,4 +1,6 @@
 import arviz
+import scipy
+import numpy as np
 
 
 def chain_convergence_diagnostics(chains, samples, param_names):
@@ -20,3 +22,47 @@ def chain_convergence_diagnostics(chains, samples, param_names):
         print(
             f'Measured Gelman-Rubin statistic for parameter "{param}": {r_hats[param]}'
         )
+
+
+def symmetric_kl_divergence(sample_1, sample_2):
+    """ """
+    # Approximate distributions using Kernel Density Estimation
+    dist1_estimate = scipy.stats.gaussian_kde(sample_1.T)
+    dist2_estimate = scipy.stats.gaussian_kde(sample_1.T)
+
+    # Monte carlo integration for KL divergence
+
+    kl_pq = np.mean(
+        np.log(dist1_estimate(sample_1.T) + 1e-10)
+        - np.log(dist2_estimate(sample_1.T) + 1e-10)
+    )
+
+    kl_pq_err = np.std(
+        np.log(dist1_estimate(sample_1.T) + 1e-10)
+        - np.log(dist2_estimate(sample_1.T) + 1e-10)
+    ) / np.sqrt(len(sample_1))
+
+    kl_qp = np.mean(
+        np.log(dist2_estimate(sample_2.T) + 1e-10)
+        - np.log(dist1_estimate(sample_2.T) + 1e-10)
+    )
+
+    kl_qp_err = np.std(
+        np.log(dist2_estimate(sample_2.T) + 1e-10)
+        - np.log(dist1_estimate(sample_2.T) + 1e-10)
+    ) / np.sqrt(len(sample_2))
+
+    return (kl_pq + kl_qp) / 2, np.sqrt(kl_pq_err**2 + kl_qp_err**2) / 2
+
+
+def kl_divergence(sample, pdf):
+    dist1_estimate = scipy.stats.gaussian_kde(sample.T)
+    kl_pq = np.mean(
+        np.log(dist1_estimate(sample.T) + 1e-10) - np.log(pdf(sample) + 1e-10)
+    )
+
+    kl_pq_err = np.std(
+        np.log(dist1_estimate(sample) + 1e-10) - np.log(pdf(sample) + 1e-10)
+    ) / np.sqrt(len(sample))
+
+    return kl_pq, kl_pq_err
